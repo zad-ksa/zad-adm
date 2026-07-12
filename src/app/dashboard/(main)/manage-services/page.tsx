700d282cd1f689c8652d7452a9ae137071ecf3c0
khaled <zad.adm.ksa@gmail.com>
2026-07-12 11:06:33 +0300

    "تعديل الخدمات"

--- src/app/dashboard/(main)/manage-services/page.tsx
+++ src/app/dashboard/(main)/manage-services/page.tsx
@@ -1,16 +1,20 @@
-import { getAllServiceTemplates } from "@/app/actions/services";
+import { getServicesForManagement, getCharitiesForSelect } from "@/app/actions/services";
 import ManageServicesClient from "./ManageServicesClient";
 
 export const metadata = {
-  title: "إدارة قوالب الخدمات",
+  title: "إدارة الخدمات",
 };
 
 export default async function ManageServicesPage() {
-  const templates = await getAllServiceTemplates();
+  const [services, charities] = await Promise.all([
+    getServicesForManagement(),
+    getCharitiesForSelect(),
+  ]);
 
   return (
     <ManageServicesClient 
-      initialTemplates={templates}
+      initialServices={services}
+      charities={charities}
     />
   );
 }
